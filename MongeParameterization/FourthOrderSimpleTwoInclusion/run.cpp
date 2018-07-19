0efa3736f903ac20d19dc2913d8f1a4eaf99dd72
kempsilonnaught <kemps@union.edu>
2018-07-19 12:48:20 -0400

    Fixing errors

--- MongeParameterization/FourthOrderSimpleTwoInclusion/run.cpp
+++ MongeParameterization/FourthOrderSimpleTwoInclusion/run.cpp
@@ -57,6 +57,14 @@ double FourthOrder::run(double r1, double r2, double sep, double x, double y, do
     	            	break;
     	        }
     	    }
+    	    for(unsigned int l_2 = 0; l_2 < GeometryInfo<2>::lines_per_cell; ++l_2){
+            	Point<2> edge_center_2 = cell -> line(l_2) -> center(true, true);
+              		if(cell -> line(l_2) -> at_boundary()){
+                		if(sqrt((std::pow((edge_center_2[0]+sep/2), 2))+std::pow(edge_center_2[1], 2)) <= (r2))
+                  			cell->set_refine_flag ();
+    	            		break;
+    	        	}
+    	    }
     	}
     	surface.execute_coarsening_and_refinement ();
     }
