1fc276d0f912d0cff29d001622c13520715a34f8
kempsilonnaught <kemps@union.edu>
2018-07-24 13:09:42 -0400

    Resustlyls

--- MongeParameterization/FourthOrderSimpleTwoInclusion/CMakeFiles/info.dir/cmake_clean.cmake
+++ MongeParameterization/FourthOrderSimpleTwoInclusion/CMakeFiles/info.dir/cmake_clean.cmake
@@ -1,8 +1,8 @@
-file(REMOVE_RECURSE
+FILE(REMOVE_RECURSE
   "CMakeFiles/info"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/info.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
+FOREACH(lang)
+  INCLUDE(CMakeFiles/info.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
