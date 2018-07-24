7809c2e36e3cc8131545a4e533c8828be80a9e06
kempsilonnaught <kemps@union.edu>
2018-07-24 13:22:52 -0400

    Weird issues from debugging process

---
+++ MongeParameterization/FourthOrderSimpleTwoInclusion/output.cpp
@@ -0,0 +1,20 @@
+#include "fourthorder.h"
+
+/*
+
+*/
+
+void FourthOrder::output(int i){
+	DataOut<2> data_out;
+
+	data_out.attach_dof_handler(doffer);
+	data_out.add_data_vector(solution, "solution");
+
+	data_out.build_patches();
+
+	std::ofstream out("gpls/surface" + std::to_string(i) + ".gpl");
+	data_out.write_gnuplot(out); 
+
+	//std::ofstream outt("matrix" + std::to_string(i) + ".txt");
+	//big_matrix.print_formatted(outt, 10, false);
+}
