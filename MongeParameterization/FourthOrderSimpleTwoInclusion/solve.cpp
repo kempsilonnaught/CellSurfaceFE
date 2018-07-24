5de64b6ada24a113d4a5fd82f1b0ba5854bb4144
kempsilonnaught <kemps@union.edu>
2018-07-24 13:54:24 -0400

    no adaptive refinement

--- MongeParameterization/FourthOrderSimpleTwoInclusion/solve.cpp
+++ MongeParameterization/FourthOrderSimpleTwoInclusion/solve.cpp
@@ -5,7 +5,7 @@
 */
 
 void FourthOrder::solve(){
-	SolverControl solver_control(50000000, 1e-14);
+	SolverControl solver_control(50000000, 1e-15);
 	SolverCG<> solver(solver_control);
 
 	solver.solve(big_matrix, solution, rhs, PreconditionIdentity());
