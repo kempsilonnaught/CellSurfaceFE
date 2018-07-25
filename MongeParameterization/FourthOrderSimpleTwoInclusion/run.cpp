083acb6f67dbf272c295efd236d8ecc73456ed84
kempsilonnaught <kemps@union.edu>
2018-07-25 13:18:34 -0400

    Fixing stuff

--- MongeParameterization/FourthOrderSimpleTwoInclusion/run.cpp
+++ MongeParameterization/FourthOrderSimpleTwoInclusion/run.cpp
@@ -17,7 +17,7 @@ written to a .gpl file, and the energy is of the surface is calculated using the
 double FourthOrder::run(double r1, double r2, double sep, double x, double y, double sigma, double kappa, double kappabar, int i){
 
 	
-	for(unsigned int refine_cycle = 0; refine_cycle < 6; ++refine_cycle){
+	for(unsigned int refine_cycle = 0; refine_cycle < 10; ++refine_cycle){
 		if(refine_cycle == 0){
 			cell_mesh(r1, r2, sep, x, y, true);
 
@@ -28,7 +28,7 @@ double FourthOrder::run(double r1, double r2, double sep, double x, double y, do
 			Vector<float> estimated_error(surface.n_active_cells());
 			KellyErrorEstimator<2>::estimate(doffer, QGauss<1>(3), typename FunctionMap<2>::type(), solution, estimated_error);
 
-			GridRefinement::refine_and_coarsen_fixed_number(surface, estimated_error, .20, 0.05, 3000);
+			GridRefinement::refine_and_coarsen_fixed_number(surface, estimated_error, .05, 0.05, 3000);
 			surface.execute_coarsening_and_refinement();
 		}
 
