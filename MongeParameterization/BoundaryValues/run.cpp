33627116f8e727d87747d83ebc3bcf8ff798a4b7
kempsilonnaught <kemps@union.edu>
2019-06-24 10:53:23 -0400

    Energy fixed! refining mesh!

--- MongeParameterization/BoundaryValues/run.cpp
+++ MongeParameterization/BoundaryValues/run.cpp
@@ -34,7 +34,7 @@ double SimulateSurface::run(double r1, double r2, double sep, double x, double y
 	cell_mesh(r1, r2, sep, x, y, true);
 	surface.refine_global(3);
 
-	for(unsigned int step=0; step<2; ++step){
+	for(unsigned int step=0; step<1; ++step){
 		Triangulation<2>::active_cell_iterator cell = surface.begin_active();
 		Triangulation<2>::active_cell_iterator endc = surface.end();
 		for(; cell!=endc; ++cell){
@@ -57,14 +57,12 @@ double SimulateSurface::run(double r1, double r2, double sep, double x, double y
 		}
 		surface.execute_coarsening_and_refinement ();
 	}
-	GridTools::remove_anisotropy(surface, 1.6180339887, 2);
-	GridTools::remove_hanging_nodes(surface, false, 14);
+	//GridTools::remove_anisotropy(surface, 1.6180339887, 2);
+	//GridTools::remove_hanging_nodes(surface, false, 14);
 
 	setup();
 	assemble(sigma, kappa, kappabar, neumann_value);
 	solve();
-
-    			                		std::cout << "HI" << std::endl;
 
 
 	std::cout << "   Number of active cells: "
