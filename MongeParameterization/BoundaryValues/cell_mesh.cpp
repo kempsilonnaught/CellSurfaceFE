9ef2dd039029d9107cdb881b917b62d7db6cd708
kempsilonnaught <kemps@union.edu>
2019-06-25 12:08:49 -0400

    Still flipping

--- MongeParameterization/BoundaryValues/cell_mesh.cpp
+++ MongeParameterization/BoundaryValues/cell_mesh.cpp
@@ -109,15 +109,15 @@ void SimulateSurface::cell_mesh(double r1, double r2, double sep, double x, doub
 
 		Triangulation<2> inclusion_1;
 		GridGenerator::hyper_cube_with_cylindrical_hole(inclusion_1, r1, sep/2, true);
-		GridTools::shift(Point<2>(2*sep, 0), inclusion_1);  
+		GridTools::shift(Point<2>(x/2 + sep/2, 0), inclusion_1);  
 
 		Triangulation<2> inclusion_2;
 		GridGenerator::hyper_cube_with_cylindrical_hole(inclusion_2, r2, sep/2, true);
-		GridTools::shift(Point<2>(sep, 0), inclusion_2);
+		GridTools::shift(Point<2>(x/2 - sep/2, 0), inclusion_2);
 
 		GridGenerator::merge_triangulations(inclusion_1, inclusion_2, surface);
 
-		HyperBallBoundary<2>* inclusion_boundary_1 = new HyperBallBoundary<2>(Point<2>(2*sep, 0), r1);
+		HyperBallBoundary<2>* inclusion_boundary_1 = new HyperBallBoundary<2>(Point<2>(x/2 + sep/2, 0), r1);
 		surface.set_boundary(5, *inclusion_boundary_1); 
 
 		for(unsigned int aa = 0; aa < 10; ++aa){
@@ -132,7 +132,7 @@ void SimulateSurface::cell_mesh(double r1, double r2, double sep, double x, doub
 					}
 		}
 
-		HyperBallBoundary<2>* inclusion_boundary_2 = new HyperBallBoundary<2>(Point<2>(sep, 0), r2);
+		HyperBallBoundary<2>* inclusion_boundary_2 = new HyperBallBoundary<2>(Point<2>(x/2 - sep/2, 0), r2);
 		surface.set_boundary(6, *inclusion_boundary_2); 
 		
 		for(unsigned int aa = 0; aa < 10; ++aa){
@@ -153,9 +153,9 @@ void SimulateSurface::cell_mesh(double r1, double r2, double sep, double x, doub
 		for (; cell != endc; ++cell){
 			for(unsigned int i = 0; i < GeometryInfo<2>::vertices_per_cell; ++i){
 				Point<2> &v = cell -> vertex(i);
-				if(v(0) == 2*sep)
+				if(v(0) == x/2 + sep)
 					v(0) = x; 
-				if(v(0) == 0)
+				if(v(0) == x/2 - sep)
 					v(0) = 0;
 				if(v(1) == sep/2)
 					v(1) = y/2; 
