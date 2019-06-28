fd119e82d24fc153779bf84bffc3634d3f537143
kempsilonnaught <kemps@union.edu>
2019-06-28 13:22:04 -0400

    fixing problem with output

--- MongeParameterization/TiltingBoundaries/output.cpp
+++ MongeParameterization/TiltingBoundaries/output.cpp
@@ -17,6 +17,7 @@ void SimulateSurface::output(int i, int j){
     data_out.add_data_vector(solution, "solution");
 
     data_out.build_patches();
+    std::cout << "J = " << j << std::endl;
 
     if(j == 1){
         std::ofstream out("gpls/positive/surface" + std::to_string(i) + "positive.gpl");
